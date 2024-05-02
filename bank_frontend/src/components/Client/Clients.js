a46227619899a120a40455208f4066fa442bfd0f
UsusCimex <d.lanin@g.nsu.ru>
2024-05-03 02:59:55 +0700

    update filter design

--- bank_frontend/src/components/Client/Clients.js
+++ bank_frontend/src/components/Client/Clients.js
@@ -99,76 +99,130 @@ function Clients() {
 
   return (
     <div className="container">
-      <h2>Клиенты</h2>
+      <h2>Clients</h2>
       {role === "ROLE_ADMIN" && (
         <form onSubmit={handleSubmitQuery} className="form-standard">
-           <div className="form-input-group">
-          <input 
-            type="text" 
-            value={baseQuery} 
-            disabled 
-            className="base-query-input form-input" />
-          <input 
-            type="text" 
-            value={userClientQuery} 
-            onChange={(e) => setUserClientQuery(e.target.value)} 
-            placeholder="e.g., WHERE 1 = 1" 
-            className="user-query-input form-input" />
-        </div>
-        <small>You can use attributes like client_id, name, email, phone, passport_data, birth_date in your WHERE clause.</small>
-        <button type="submit" className="form-button">Выполнить запрос</button>
+          <div className="form-query-group">
+            <input 
+              type="text" 
+              value={baseQuery} 
+              disabled 
+              className="base-query-input" />
+            <input 
+              type="text" 
+              value={userClientQuery} 
+              onChange={(e) => setUserClientQuery(e.target.value)} 
+              placeholder="e.g., WHERE 1 = 1" 
+              className="user-query-input" />
+          </div>
+          <small>You can use attributes like client_id, name, email, phone, passport_data, birth_date in your WHERE clause.</small>
+          <button type="submit" className="execute-query-button">Execute Query</button>
         </form>
       )}
       <form onSubmit={handleApplyFilters} className="filter-form">
-        {Object.keys(filters).map(key => (
-          <div key={key} className="form-input-group">
-            {key !== 'creditStatus' && key !== 'hasCredit' && key !== 'isBlocked' && (
-              <input
-                type={key === 'birthDate' ? 'date' : 'text'}
-                name={key}
-                value={filters[key] || ''}
-                placeholder={`Фильтр по ${key}`}
-                onChange={handleFilterChange}
-                className="form-input"
-              />
-            )}
-            {key === 'creditStatus' && (
-              <select name="creditStatus" value={filters[key] || ''} onChange={handleFilterChange} className="form-input">
-                <option value="">Выберите статус кредита</option>
-                <option value="Expired">Expired</option>
-                <option value="Active">Active</option>
-                <option value="Closed">Closed</option>
-              </select>
-            )}
-            {key === 'hasCredit' && (
-              <select name="hasCredit" value={filters[key]} onChange={handleFilterChange} className="form-input">
-                <option value="">Выберите кредитный статус</option>
-                <option value={true}>Есть</option>
-                <option value={false}>Нет</option>
-              </select>
-            )}
-            {key === 'isBlocked' && (
-              <select name="isBlocked" value={filters[key]} onChange={handleFilterChange} className="form-input">
-                <option value="">Выберите статус блокировки</option>
-                <option value={true}>Заблокирован</option>
-                <option value={false}>Не заблокирован</option>
-              </select>
-            )}
+        <div className="form-container">
+          <div className="form-input-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={filters.name}
+              placeholder="Enter name"
+              onChange={handleFilterChange}
+              className="form-input"
+            />
           </div>
-        ))}
-        <button type="submit" className="form-button">Применить фильтры</button>
+          <div className="form-input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={filters.email}
+              placeholder="Enter email"
+              onChange={handleFilterChange}
+              className="form-input"
+            />
+          </div>
+          <div className="form-input-group">
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              value={filters.phone}
+              placeholder="Enter phone"
+              onChange={handleFilterChange}
+              className="form-input"
+            />
+          </div>
+          <div className="form-input-group">
+            <label htmlFor="passport">Passport</label>
+            <input
+              type="text"
+              id="passport"
+              name="passport"
+              value={filters.passport}
+              placeholder="Enter passport number"
+              onChange={handleFilterChange}
+              className="form-input"
+            />
+          </div>
+          <div className="form-input-group">
+            <label htmlFor="birthDate">Birth Date</label>
+            <input
+              type="date"
+              id="birthDate"
+              name="birthDate"
+              value={filters.birthDate}
+              onChange={handleFilterChange}
+              className="form-input"
+            />
+          </div>
+          <div className="form-input-group">
+            <label htmlFor="creditStatus">Credit Status</label>
+            <select id="creditStatus" name="creditStatus" value={filters.creditStatus || ''} onChange={handleFilterChange} className="form-input">
+              <option value="">Select status</option>
+              <option value="Expired">Expired</option>
+              <option value="Active">Active</option>
+              <option value="Closed">Closed</option>
+            </select>
+          </div>
+          <div className="form-input-group">
+            <label htmlFor="hasCredit">Has Credit?</label>
+            <select id="hasCredit" name="hasCredit" value={filters.hasCredit} onChange={handleFilterChange} className="form-input">
+              <option value="">Select option</option>
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
+          </div>
+          <div className="form-input-group">
+            <label htmlFor="isBlocked">Is Blocked?</label>
+            <select id="isBlocked" name="isBlocked" value={filters.isBlocked} onChange={handleFilterChange} className="form-input">
+              <option value="">Select option</option>
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
+          </div>
+        </div>
+        <button type="submit" className="form-button">Search</button>
       </form>
-      {(role === "ROLE_OPERATOR" || role === "ROLE_ADMIN") && <button onClick={() => setModalOpen(true)} className="button button-primary">Добавить клиента</button>}
+      {(role === "ROLE_OPERATOR" || role === "ROLE_ADMIN") && (
+        <div className="button-group">
+          <button onClick={() => setModalOpen(true)} className="add-button">Add Client</button>
+        </div>
+      )}
       {modalOpen && <AddClientModal onClose={() => setModalOpen(false)} onClientAdded={fetchClients} />}
-      {loading && <div>Загрузка...</div>}
-      {error && <div className="alert-danger">Ошибка: {error}</div>}
+      {loading && <div>Loading...</div>}
+      {error && <div className="alert-danger">Error: {error}</div>}
       <div className="list-container">
         {clients.map(client => (
           <div key={client.id} className="item-card" onClick={() => navigate(`/clients/${client.id}`)}>
-            <p>Имя: {client.name}</p>
+            <p>Name: {client.name}</p>
             <p>Email: {client.email}</p>
-            <p>Телефон: {client.phone}</p>
-            <p>Дата рождения: {client.birthDate && new Date(client.birthDate).toLocaleDateString()}</p>
+            <p>Phone: {client.phone}</p>
+            <p>Birth Date: {client.birthDate && new Date(client.birthDate).toLocaleDateString()}</p>
           </div>
         ))}
       </div>
